1f37a8d8b7c90f60fc61f759f1da7ca49aa7130d
Нуради <nuradi@MacBook-Air-Nuradi.local>
2026-02-20 09:59:49 +0300

    add repo for team

--- Match_service/internal/dto/dto_team.go
+++ Match_service/internal/dto/dto_team.go
@@ -15,6 +15,5 @@ type TeamListResponse struct {
 	Total      int64         `json:"total"`
 	Page       int           `json:"page"`
 	PageSize   int           `json:"page_size"`
-	TotalPages int           `json:"total_pages"`
 }
 
